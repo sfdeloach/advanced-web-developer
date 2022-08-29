e118e7afc2a291a9e3bee23b43bb79e76f90ecc9
Steven DeLoach <sfdeloach@gmail.com>
2022-08-29 15:25:30 -0400

    update lesson 231 with notes

--- 20-D3-datajoins-updatepatterns/231-basic-data-join.js
+++ 20-D3-datajoins-updatepatterns/231-basic-data-join.js
@@ -1,143 +1,165 @@
 var characters = [
   {
     id: 1,
-    name: 'Rick Sanchez',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
     origin: {
-      name: 'Earth (C-137)',
-      url: 'https://rickandmortyapi.com/api/location/1'
+      name: "Earth (C-137)",
+      url: "https://rickandmortyapi.com/api/location/1",
     },
     location: {
-      name: 'Citadel of Ricks',
-      url: 'https://rickandmortyapi.com/api/location/3'
+      name: "Citadel of Ricks",
+      url: "https://rickandmortyapi.com/api/location/3",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
 
-    url: 'https://rickandmortyapi.com/api/character/1',
-    created: '2017-11-04T18:48:46.250Z'
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
   },
   {
     id: 2,
-    name: 'Morty Smith',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
     origin: {
-      name: 'unknown',
-      url: ''
+      name: "unknown",
+      url: "",
     },
     location: {
-      name: 'Citadel of Ricks',
-      url: 'https://rickandmortyapi.com/api/location/3'
+      name: "Citadel of Ricks",
+      url: "https://rickandmortyapi.com/api/location/3",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/2',
-    created: '2017-11-04T18:50:21.651Z'
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    url: "https://rickandmortyapi.com/api/character/2",
+    created: "2017-11-04T18:50:21.651Z",
   },
   {
     id: 3,
-    name: 'Summer Smith',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Female',
+    name: "Summer Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female",
     origin: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
     location: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/3',
-    created: '2017-11-04T19:09:56.428Z'
+    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    url: "https://rickandmortyapi.com/api/character/3",
+    created: "2017-11-04T19:09:56.428Z",
   },
   {
     id: 4,
-    name: 'Beth Smith',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Female',
+    name: "Beth Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female",
     origin: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
     location: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/4',
-    created: '2017-11-04T19:22:43.665Z'
+    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    url: "https://rickandmortyapi.com/api/character/4",
+    created: "2017-11-04T19:22:43.665Z",
   },
   {
     id: 5,
-    name: 'Jerry Smith',
-    status: 'Alive',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
+    name: "Jerry Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
     origin: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
     location: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/5',
-    created: '2017-11-04T19:26:56.301Z'
+    image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    url: "https://rickandmortyapi.com/api/character/5",
+    created: "2017-11-04T19:26:56.301Z",
   },
   {
     id: 6,
-    name: 'Abadango Cluster Princess',
-    status: 'Alive',
-    species: 'Alien',
-    type: '',
-    gender: 'Female',
+    name: "Abadango Cluster Princess",
+    status: "Alive",
+    species: "Alien",
+    type: "",
+    gender: "Female",
     origin: {
-      name: 'Abadango',
-      url: 'https://rickandmortyapi.com/api/location/2'
+      name: "Abadango",
+      url: "https://rickandmortyapi.com/api/location/2",
     },
     location: {
-      name: 'Abadango',
-      url: 'https://rickandmortyapi.com/api/location/2'
+      name: "Abadango",
+      url: "https://rickandmortyapi.com/api/location/2",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/6.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/6',
-    created: '2017-11-04T19:50:28.250Z'
+    image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    url: "https://rickandmortyapi.com/api/character/6",
+    created: "2017-11-04T19:50:28.250Z",
   },
   {
     id: 7,
-    name: 'Abradolf Lincler',
-    status: 'unknown',
-    species: 'Human',
-    type: 'Genetic experiment',
-    gender: 'Male',
+    name: "Abradolf Lincler",
+    status: "unknown",
+    species: "Human",
+    type: "Genetic experiment",
+    gender: "Male",
     origin: {
-      name: 'Earth (Replacement Dimension)',
-      url: 'https://rickandmortyapi.com/api/location/20'
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
     },
     location: {
-      name: 'Testicle Monster Dimension',
-      url: 'https://rickandmortyapi.com/api/location/21'
+      name: "Testicle Monster Dimension",
+      url: "https://rickandmortyapi.com/api/location/21",
     },
-    image: 'https://rickandmortyapi.com/api/character/avatar/7.jpeg',
-    url: 'https://rickandmortyapi.com/api/character/7',
-    created: '2017-11-04T19:59:20.523Z'
-  }
+    image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+    url: "https://rickandmortyapi.com/api/character/7",
+    created: "2017-11-04T19:59:20.523Z",
+  },
 ];
 
-d3.select('#characters')
-  .selectAll('p')
-  .data(characters)
-  .enter()
-  .append('p')
-  .text(d => d.name);
+// Before executing the chain of methods, let's look at the object returned at each step
+
+console.warn("A selection under the groups object, consisting of div#characters");
+console.log(d3.select("#characters"));
+
+console.warn("The selection under the groups object now consists of an empty NodeList");
+console.log(d3.select("#characters").selectAll("p"));
+
+console.warn(`Enter and exit objects are attached to the selection, also an empty array
+the same size as the data is now attached under the groups object`);
+console.log(
+  d3
+    .select("#characters")
+    .selectAll("p") // there are no p elements, but D3 still returns a selection (see above)
+    .data(characters)
+);
+
+console.warn(`The Enter and exit objects are no longer attached to the selection, the groups
+object now consists of seven p elements that match with the data`);
+console.log(
+  d3
+    .select("#characters")
+    .selectAll("p")
+    .data(characters)
+    .enter()
+    .append("p")
+    .text((d, i) => `${i} - ${d.name}`) // returning a message using the index and data
+);
