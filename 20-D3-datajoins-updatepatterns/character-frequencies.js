7f8d0ed27666fe74b6f43d71abdba53d6a9bbc6d
Steven DeLoach <sfdeloach@gmail.com>
2022-09-18 09:34:06 -0400

    char freq final

--- 20-D3-datajoins-updatepatterns/character-frequencies.js
+++ 20-D3-datajoins-updatepatterns/character-frequencies.js
@@ -1,7 +1,11 @@
+// The charFreq uses an object to keep track of unique character values. When called by D3 to
+// produce its data (get characterFrequency), the object is converted to an array.
 class CharacterFrequency {
+  // private
   #charFreq = {};
   #newChars = 0;
 
+  // getter that transforms an object to an array
   get characterFrequency() {
     const resultArray = [];
 
@@ -20,6 +24,8 @@ class CharacterFrequency {
     return this.#newChars;
   }
 
+  // Iterate over enumerable keys of the existing #charFreq object to see if it already contains
+  // characters in the newly provided string, then iterate over the word to count occurrences.
   updateCharFreq(w) {
     this.#newChars = 0;
 
@@ -86,11 +92,15 @@ document.querySelector("form").onsubmit = (event) => {
     .style("height", (d) => d.count * 24 + "px")
     .text((d) => d.char);
 
+  // show the graph and clear the input field
   document.querySelector(".results").classList.remove("hide");
   letters.value = "";
 };
 
+// Reset the form, create a new reference to the character frequency object, remove the data set
+// from the D3 object, and hide the graph portion of the page
 document.querySelector("form > #reset").addEventListener("click", (event) => {
   cf = new CharacterFrequency();
+  d3.select("#graph").selectAll("div").data([]).exit().remove();
   document.querySelector(".results").classList.add("hide");
 });
