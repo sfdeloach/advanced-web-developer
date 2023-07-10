c1052f4055bee464fa1628a1121e2f17823f3cc2
Steven DeLoach <sfdeloach@gmail.com>
2023-07-09 22:24:44 -0400

    memory game complete

--- 28-events-forms/memory-game/src/App.jsx
+++ 28-events-forms/memory-game/src/App.jsx
@@ -3,55 +3,82 @@ import { CARD_COLORS } from './assets/settings';
 import Navbar from './Navbar';
 import Game from './Game';
 
-// TODO: add randomness to colorValue assignment
-function createCards(quantity = 16, numColors = CARD_COLORS.length) {
-  const result = [];
+function App() {
+  const [cards, setCards] = useState(createCards());
+  const [selected, setSelected] = useState([]);
 
-  for (let i = 0; i < quantity; ++i) {
-    result.push({
-      id: i,
-      colorValue: i % numColors,
-      isVisible: false,
-      isMatched: false
+  // setCards() will cause re-renders to occur, however, it will not update the reference to cards,
+  // which is an advantage as it is unnecessary to make a copy
+  useEffect(() => {
+    if (selected.length === 2) {
+      // freeze cards, this will cause all cards to temporarily ignore mouse clicks, but it will
+      // not update the reference to cards just yet, as you will see, setCards() is called a few
+      // more times in this closure and the reference to cards is referring to its original state
+      setCards(
+        cards.map(card => {
+          return { ...card, isEnabled: false };
+        })
+      );
+
+      if (selected[0].colorValue !== selected[1].colorValue) {
+        setTimeout(
+          () =>
+            setCards(
+              cards.map(card =>
+                card.id === selected[0].id || card.id === selected[1].id
+                  ? { ...card, isVisible: false, isEnabled: true }
+                  : card
+              )
+            ),
+          1000
+        );
+      } else {
+        setCards([...cards]);
+      }
+
+      // clear selected cards
+      setSelected([]);
+    }
+  }, [selected]);
+
+  function handleCardClick(id) {
+    setCards(
+      cards.map(card => (card.id === id ? { ...card, isVisible: true, isEnabled: false } : card))
+    );
+
+    setSelected([...selected, cards[id]]);
+  }
+
+  function getRandomInt(max, min = 0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  function createCards(quantity = 16, numColors = CARD_COLORS.length) {
+    const topCards = new Set(),
+      bottomCards = new Set();
+
+    while (topCards.size < numColors) {
+      topCards.add(getRandomInt(numColors));
+    }
+
+    while (bottomCards.size < numColors) {
+      bottomCards.add(getRandomInt(numColors));
+    }
+
+    return [...topCards, ...bottomCards].map((colorNumber, idx) => {
+      return {
+        colorValue: colorNumber,
+        id: idx,
+        isEnabled: true,
+        isVisible: false
+      };
     });
   }
 
-  return result;
-}
-
-function App() {
-  const [cards, setCards] = useState(createCards());
-  const [exposedCards, setExposedCards] = useState([]);
-
-  useEffect(() => {
-    if (exposedCards.length == 2) {
-      const cardsCopy = [...cards];
-      if (exposedCards[0].colorValue == exposedCards[1].colorValue) {
-        console.log('match');
-        cardsCopy[exposedCards[0].id].isMatched = true;
-        cardsCopy[exposedCards[1].id].isMatched = true;
-        setCards(cardsCopy);
-      } else {
-        console.log('not a match');
-        cardsCopy[exposedCards[0].id].isVisible = false;
-        cardsCopy[exposedCards[1].id].isVisible = false;
-        setCards(cardsCopy);
-      }
-      setExposedCards([]);
-    }
-  }, [exposedCards]);
-
   function resetGame() {
     setCards(createCards());
-  }
-
-  function handleCardClick(id) {
-    if (exposedCards.length < 2) {
-      setExposedCards([...exposedCards, cards[id]]);
-      const cardsCopy = [...cards];
-      cardsCopy[id].isVisible = true;
-      setCards(cardsCopy);
-    }
   }
 
   return (
--- 28-events-forms/memory-game/src/Card.jsx
+++ 28-events-forms/memory-game/src/Card.jsx
@@ -3,16 +3,19 @@ import { COLORS, CARD_COLORS } from './assets/settings';
 function Card({ card, handleCardClick }) {
   return (
     <div
+      className='card'
       style={{
-        width: '120px',
+        width: '100px',
         height: '160px',
         backgroundColor: card.isVisible ? CARD_COLORS[card.colorValue] : COLORS.cardBack,
+        border: `8px solid ${COLORS.cardBack}`,
         borderRadius: '12px',
         margin: '8px',
-        cursor: 'pointer'
+        cursor: card.isEnabled ? 'pointer' : 'not-allowed',
+        transition: !card.isVisible && 'background-color 0.8s ease-out'
       }}
-      onClick={() => !card.isVisible && handleCardClick(card.id)}
-    ></div>
+      onClick={() => card.isEnabled && handleCardClick(card.id)}
+    />
   );
 }
 
